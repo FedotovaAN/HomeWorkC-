f7b87fec216c4aaaeb23a7436a6e04d1acceafd9
Alla Fedotova <fedotovaan@bk.ru>
2023-10-02 22:59:48 +0300

    added file TASK1

---
+++ TASK1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее;
+
+Console.WriteLine("You first number:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("You second number:");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine($"max = {a} ");
+    Console.WriteLine($"min = {b} ");
+}
+else
+{
+   Console.WriteLine($"min = {a}");
+   Console.WriteLine($"max = {b} ");
+}
+
